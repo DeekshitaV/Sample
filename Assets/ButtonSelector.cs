4aa441d20998f3ec9b5850e6b4d53bbda941e666
DeekshitaV <deekshitaverma@gmail.com>
2020-09-19 15:45:21 +0530

    Initial commit

---
+++ Assets/ButtonSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ButtonSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] speakers;
+    private GameObject selectedspeaker;
+    void Start()
+    {
+        selectedspeaker = speakers[0];
+        selectedspeaker.SetActive(true);
+    }
+
+    // Update is called once per frame
+
+    public void Selector( GameObject clicked)
+    {
+        foreach( GameObject go in speakers)
+        {
+            go.SetActive(false);
+        }
+        selectedspeaker = clicked;
+        selectedspeaker.SetActive(true);
+    }
+
+    
+}
---
+++ Assets/PlayPause.cs
@@ -0,0 +1,20 @@
+﻿//using System.Collections;
+/*using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPause : MonoBehaviour
+{
+    public AudioSource audio;
+    public void Pause( )
+    {
+        Time.timeScale = 0;
+        audio.Pause();
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;
+        audio.Play();
+    }
+
+}*/
